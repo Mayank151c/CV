ea645be0cb74f421c9f5988225611f053d13bec7
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-05-28 23:07:49 +0530

    Redesigning about section with card-deck and adding two pictures

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ window.onmouseover = function()
 	var img = document.getElementByTagName("img");
 	img.style.border = "2px solid red";
 }
+
+var codeforcesHTML;
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://codeforces.com/profile/mayank_010501", true);
+  xhttp.send();
+}
